d7bcfc36f5930bfd6ecd8ab6e8dbee543fce2eee
Douglas <douglasgia@hotmail.com>
2025-07-09 18:27:28 -0300

    feat: cria model

---
+++ src/models/Municipio.ts
@@ -0,0 +1,10 @@
+
+export interface Municipio {
+  id: number;
+  estado: string;
+  codigo_estado: number;
+  codigo_municipio: number;
+  nome_municipio: string;
+  capital: boolean;
+  populacao: number;
+}
