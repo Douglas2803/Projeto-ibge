2509ba6d2743a3856046f31622e0a7649335f74b
Douglas <douglasgia@hotmail.com>
2025-07-09 18:29:04 -0300

    feat: cria controllers

---
+++ src/controllers/MunicipioController.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from 'express';
+import MunicipioService from '../services/MunicipioService';
+
+class MunicipioController {
+  findByNome = async (req: Request, res: Response) => {
+    const { nome } = req.query;
+    const municipio = await MunicipioService.findByNome(nome as string);
+    res.json(municipio);
+  }
+
+  getPopulacaoByEstado = async (req: Request, res: Response) => {
+    const { uf } = req.params;
+    const populacao = await MunicipioService.getPopulacaoByEstado(uf);
+    res.json({ estado: uf, populacao });
+  }
+
+  listCapitais = async (req: Request, res: Response) => {
+    const capitais = await MunicipioService.listCapitais();
+    res.json(capitais);
+  }
+
+  listMunicipiosByPopulacao = async (req: Request, res: Response) => {
+    const { acima, min, max } = req.query;
+    if (acima) {
+      const municipios = await MunicipioService.listMunicipiosByPopulacao(Number(acima));
+      return res.json(municipios);
+    }
+    if (min && max) {
+      const municipios = await MunicipioService.listMunicipiosByPopulacao(Number(min), Number(max));
+      return res.json(municipios);
+    }
+    res.status(400).json({ error: 'Parâmetros inválidos' });
+  }
+
+  getEstadosOndeCapitalNaoEMaisPopulosa = async (req: Request, res: Response) => {
+    const estados = await MunicipioService.getEstadosOndeCapitalNaoEMaisPopulosa();
+    res.json(estados);
+  }
+
+  getDezMunicipiosMaisPopulososNaoCapitais = async (req: Request, res: Response) => {
+    const municipios = await MunicipioService.getDezMunicipiosMaisPopulososNaoCapitais();
+    res.json(municipios);
+  }
+}
+
+export default new MunicipioController();
