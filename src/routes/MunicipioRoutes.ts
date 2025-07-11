66485852e82c06f4386a4a43ae4ba976cd19e2c2
Douglas <douglasgia@hotmail.com>
2025-07-10 21:11:14 -0300

    refactor: altera arquivos

--- src/routes/MunicipioRoutes.ts
+++ src/routes/MunicipioRoutes.ts
@@ -1,14 +1,14 @@
 
 import { Router } from 'express';
-import MunicipioController from '../controllers/MunicipioController';
+import municipioController from '../controllers/MunicipioController';
 
 const router = Router();
 
-router.get('/municipios', MunicipioController.findByNome);
-router.get('/estados/:uf/populacao', MunicipioController.getPopulacaoByEstado);
-router.get('/capitais', MunicipioController.listCapitais);
-router.get('/municipios/populacao', MunicipioController.listMunicipiosByPopulacao);
-router.get('/estados/capital-nao-mais-populosa', MunicipioController.getEstadosOndeCapitalNaoEMaisPopulosa);
-router.get('/municipios/mais-populosos-nao-capitais', MunicipioController.getDezMunicipiosMaisPopulososNaoCapitais);
+router.get('/municipios', municipioController.findByNome);
+router.get('/estados/:uf/populacao', municipioController.getPopulacaoByEstado);
+router.get('/capitais', municipioController.listCapitais);
+router.get('/municipios/populacao', municipioController.listMunicipiosByPopulacao);
+router.get('/estados/capital-nao-mais-populosa', municipioController.getEstadosOndeCapitalNaoEMaisPopulosa);
+router.get('/municipios/mais-populosos-nao-capitais', municipioController.getDezMunicipiosMaisPopulososNaoCapitais);
 
 export default router;
